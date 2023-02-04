10e2cfd83cffd8b21dfbd3a7d1c4c29dcb3bffce
linus-rc <linus.rudbeck@distansakademin.se>
2023-02-04 13:55:56 +0100

    Added search functionality

---
+++ BroadcastSocialMedia/BroadcastSocialMedia/ViewModels/UsersIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BroadcastSocialMedia.Models;
+
+namespace BroadcastSocialMedia.ViewModels
+{
+    public class UsersIndexViewModel
+    {
+        public string Search { get; set; }
+        public List<ApplicationUser> Result { get; set; } = new List<ApplicationUser>();
+    }
+}
