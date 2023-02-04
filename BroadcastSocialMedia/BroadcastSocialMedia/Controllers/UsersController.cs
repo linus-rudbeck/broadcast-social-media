6f62fad7d3715b05f67f2f57e250f77ca28608ac
linus-rc <linus.rudbeck@distansakademin.se>
2023-02-04 15:55:19 +0100

    Added show Broadcasts

--- BroadcastSocialMedia/BroadcastSocialMedia/Controllers/UsersController.cs
+++ BroadcastSocialMedia/BroadcastSocialMedia/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using BroadcastSocialMedia.Data;
+using BroadcastSocialMedia.Models;
 using BroadcastSocialMedia.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,10 +10,12 @@ namespace BroadcastSocialMedia.Controllers
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public UsersController(ApplicationDbContext dbContext)
+        public UsersController(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index(UsersIndexViewModel viewModel)
@@ -23,6 +27,23 @@ namespace BroadcastSocialMedia.Controllers
                                 .ToListAsync();
                 viewModel.Result = users;
             }
+
+            return View(viewModel);
+        }
+
+        [Route("/Users/{id}")]
+        public async Task<IActionResult> ShowUser(string id)
+        {
+            var broadcasts = await _dbContext.Broadcasts.Where(b => b.User.Id == id)
+                .OrderByDescending(b => b.Published)
+                .ToListAsync();
+            var user = await _userManager.GetUserAsync(User);
+
+            var viewModel = new UsersShowUserViewModel()
+            {
+                Broadcasts = broadcasts,
+                User = user
+            };
 
             return View(viewModel);
         }
