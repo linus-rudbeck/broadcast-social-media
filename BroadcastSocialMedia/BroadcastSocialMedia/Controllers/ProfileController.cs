d68c5175982ac8d82d433b8576e36491e26aa288
linus-rc <linus.rudbeck@distansakademin.se>
2023-02-04 13:28:05 +0100

    Added Name to User profile

---
+++ BroadcastSocialMedia/BroadcastSocialMedia/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿using BroadcastSocialMedia.Models;
+using BroadcastSocialMedia.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BroadcastSocialMedia.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var viewModel = new ProfileIndexViewModel()
+            {
+                Name = user.Name ?? ""
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ProfileIndexViewModel viewModel)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            user.Name = viewModel.Name;
+
+            await _userManager.UpdateAsync(user);
+
+            return Redirect("/");
+        }
+    }
+}
